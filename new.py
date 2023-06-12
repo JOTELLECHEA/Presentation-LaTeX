e8b161f8de2a230bcccc4ac85b430ba239e4dd4e
jonathanTellechea <jtellece@ucsc.edu>
2023-06-12 11:14:10 -0700

    update

--- new.py
+++ new.py
@@ -3,6 +3,7 @@ import shutil
 home =  os.getcwd()
 temp = os.path.join(home,'template.tex')
 make = os.path.join(home,'Makefile')
+logo = os.path.join(home,'logo.pdf')
 if os.path.exists('template.tex') == True:
 	print('template.tex found\n')
 	f = input('Name of new presentation: ')
@@ -12,6 +13,7 @@ if os.path.exists('template.tex') == True:
 	os.mkdir(path)
 	shutil.copy(temp, path)
 	shutil.copy(make, path)
+	shutil.copy(logo, path)
 	print('Done!')
 else:
 	print('template.tex not found!')
